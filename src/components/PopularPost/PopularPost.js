c64942a99816ca21e0c3a551ae3c1190ec86c1b5
Vishw Kadu <68730174+vk1511@users.noreply.github.com>
2021-01-02 03:20:16 +0530

    PopularPost.js
    change variable name and set the default img. because API doesn't provide img

--- src/components/PopularPost/PopularPost.js
+++ src/components/PopularPost/PopularPost.js
@@ -12,24 +12,24 @@ const PopularPost = () => {
         return (
           <div className="popular-post-container" key={index}>
             <div className="popular-post-img">
-              <Link to={`/specific-blog/${item.id}`}>
-                <img src={item.thumb} alt="img" className="popular-image" />
+              <Link to={`/specific-blog/${item._id}`}>
+                <img src="https://images.unsplash.com/photo-1609507719752-c5ee71ef1705?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="img" className="popular-image" />
               </Link>
             </div>
             <div className="popular-post-details">
               <div className="popular-article-detail">
                 <p className="popular-article-catagory">
-                  Technology &nbsp; Social
+                  {item.blogCategory}
                 </p>
               </div>
               <Link
-                to={`/specific-blog/${item.id}`}
+                to={`/specific-blog/${item._id}`}
                 className="pop-specific-link"
               >
-                <h4>{item.title}</h4>
+                <h4>{item.blogTitle}</h4>
               </Link>
               <p className="popular-description">{item.content}</p>
-              <p>1 Dec • 1 min</p>
+              <p>1 Dec • {item.xMinsRead} min Read</p>
             </div>
           </div>
         );
