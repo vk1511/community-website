d67f99373be5dc589f3e72cdc41e313a07e187b6
Vishw Kadu <68730174+vk1511@users.noreply.github.com>
2021-01-02 03:24:01 +0530

    SomeBlog.js
    Variable name change

--- src/components/SomeBlogs/SomeBlogs.js
+++ src/components/SomeBlogs/SomeBlogs.js
@@ -14,20 +14,20 @@ const SomeBlogs = () => {
             <div className="some-post-details">
               <div className="some-article-detail">
                 <p className="some-article-catagory">
-                  Technology &nbsp; Social
+                   {item.blogCategory}
                 </p>
               </div>
               <Link
-                to={`/specific-blog/${item.id}`}
+                to={`/specific-blog/${item._id}`}
                 className="pop-specific-link"
               >
-                <h4>{item.title}</h4>
-                <p className="some-description">{item.content}</p>
-                <p>1 Dec • 1 min Read</p>
+                <h4>{item.blogTitle}</h4>
+                <p className="some-description">{item.blogContent}</p>
+                <p>1 Dec • {item.xMinsRead} min Read</p>
               </Link>
             </div>
             <div className="some-post-img">
-              <Link to={`/specific-blog/${item.id}`}>
+              <Link to={`/specific-blog/${item._id}`}>
                 <img src={item.thumb} alt="img" className="some-image" />
               </Link>
             </div>
